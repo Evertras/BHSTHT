480691efebaed11a7d24775adafb4bb5af66c839
Evertras <bfullj@gmail.com>
2015-03-09 15:38:09 -0700

    More hero implementation

---
+++ HSData/HSData/BoardEntities/Hero.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Math;
+
+namespace HSData
+{
+    /// <summary>
+    /// A hero that belongs to the player
+    /// </summary>
+    public class Hero : IBoardEntity
+    {
+        public Hero(int initialHealth, int maxHealth, int currentHealth)
+        {
+            InitialHealth = initialHealth;
+            MaxHealth = maxHealth;
+            CurrentHealth = currentHealth;
+        }
+
+        public int CurrentHealth { get; }
+
+        public int InitialHealth { get; }
+
+        public int MaxHealth { get; }
+
+        public IBoardEntity Damage(int damageAmount)
+        {
+            if (damageAmount <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Damage must be a positive integer greater than zero");
+            }
+
+            return new Hero(InitialHealth, MaxHealth, CurrentHealth - damageAmount);
+        }
+
+        public IBoardEntity Heal(int healAmount)
+        {
+            if (healAmount <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Healing must be a positive integer greater than zero");
+            }
+
+            return new Hero(InitialHealth, MaxHealth, Min(MaxHealth, CurrentHealth + healAmount));
+        }
+    }
+}
--- HSData/HSDataTest/HeroTest.cs
+++ HSData/HSDataTest/HeroTest.cs
@@ -12,11 +12,58 @@ namespace HSDataTest
         [TestMethod]
         public void HeroCanBeDamaged()
         {
+            const int damageAmount = 5;
             IBoardEntity hero = new Hero(initialHP, initialHP, initialHP);
 
-            hero = hero.Damage(5);
+            hero = hero.Damage(damageAmount);
 
-            Assert.AreEqual(initialHP - 5, hero.CurrentHealth);
+            Assert.AreEqual(initialHP - damageAmount, hero.CurrentHealth);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void HeroCannotBeDamagedForNegative()
+        {
+            const int negativeDamageAmount = -5;
+            IBoardEntity hero = new Hero(initialHP, initialHP, initialHP);
+
+            hero = hero.Damage(negativeDamageAmount);
+
+            Assert.Fail("Shouldn't have gotten here");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void HeroCannotBeDamagedForZero()
+        {
+            const int zeroDamage = 0;
+            IBoardEntity hero = new Hero(initialHP, initialHP, initialHP);
+
+            hero = hero.Damage(zeroDamage);
+
+            Assert.Fail("Shouldn't have gotten here");
+        }
+
+        [TestMethod]
+        public void HeroCanBeHealed()
+        {
+            const int healAmount = 5;
+            IBoardEntity hero = new Hero(initialHP, initialHP, initialHP - healAmount);
+
+            hero = hero.Heal(healAmount);
+
+            Assert.AreEqual(initialHP, hero.CurrentHealth);
+        }
+
+        [TestMethod]
+        public void HeroCantBeHealedPastMaxHealth()
+        {
+            const int healAmount = 5;
+            IBoardEntity hero = new Hero(initialHP, initialHP, initialHP - (healAmount - 1));
+
+            hero = hero.Heal(healAmount);
+
+            Assert.AreEqual(initialHP, hero.CurrentHealth);
         }
     }
 }
