d79b6a9ecada82de65745ffc930d7f54ec5c5eba
Evertras <bfullj@gmail.com>
2015-03-10 16:08:31 -0700

    Added a message event

---
+++ HSData/HSData/GameEvents/GameEventMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Localization;
+
+namespace HSData
+{
+    public class GameEventMessage : IGameEvent
+    {
+        public GameEventMessage(string messageKey)
+        {
+            MessageKey = messageKey;
+        }
+
+        public string MessageKey { get; }
+
+        public IBoardState Apply(IBoardState initialState)
+        {
+            // No actual change
+            return initialState;
+        }
+
+        public LocalizedString Describe(IBoardState boardState, ILocalizer localizer)
+        {
+            return localizer.Localize(MessageKey);
+        }
+    }
+}
