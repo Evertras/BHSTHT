fdfa9c540d1acd69adb61add13630a1ea46ae176
Evertras <bfullj@gmail.com>
2015-03-10 16:12:38 -0700

    Added restore mana event

---
+++ HSData/HSData/CardEffects/CardEffectActiveHeroRestoreMana.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSData
+{
+    public class CardEffectActiveHeroRestoreMana : CardEffectUntargeted
+    {
+        public CardEffectActiveHeroRestoreMana(int amount)
+        {
+            Amount = amount;
+        }
+
+        public int Amount { get; }
+
+        public override IGameEvent GenerateEvent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HSData/HSData/GameEvents/GameEventActiveHeroRestoresMana.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Localization;
+
+namespace HSData
+{
+    public class GameEventActiveHeroRestoresMana : IGameEvent
+    {
+        public GameEventActiveHeroRestoresMana(int amount)
+        {
+            Amount = amount;
+        }
+
+        public int Amount { get; }
+
+        public IBoardState Apply(IBoardState initialState)
+        {
+            var alteredHero = initialState.ActivePlayerState.Hero.Heal(Amount);
+
+            return initialState.AlterActivePlayer(initialState.ActivePlayerState.AlterHero(alteredHero));
+        }
+
+        public LocalizedString Describe(IBoardState boardState, ILocalizer localizer)
+        {
+            return localizer.LocalizeFormat("HeroHealed", boardState.ActivePlayerState.BattleTag, Amount);
+        }
+    }
+}
